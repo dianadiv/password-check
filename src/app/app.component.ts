aa6d69b2dee51b8c39b22300c0c0620a068a35ee
dianadiv <diana.gs202@gmail.com>
2023-10-21 17:07:35 +0300

    add main logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'password-check';
+
+  password = '';
+  showPassword = false;
+  colors = ["grey", "grey", "grey"];
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  reset() {
+    this.password = '';
+    this.colors = ["grey", "grey", "grey"]
+  }
+
+  checkPassword() {
+    if (this.password.length === 0) {
+      this.colors = ["grey", "grey", "grey"];
+    } else if (this.password.length < 8) {
+      this.colors = ["red", "red", "red"];
+    } else if (/[a-zA-Z]/.test(this.password) && /[0-9]/.test(this.password) && /[!@#$%^&*]/.test(this.password)) {
+      this.colors = ["green", "green", "green"];
+    } else if (/[a-zA-Z]/.test(this.password)
+        && /[0-9]/.test(this.password) || /[a-zA-Z]/.test(this.password)
+        && /[!@#$%^&*]/.test(this.password) || /[0-9]/.test(this.password)
+        && /[!@#$%^&*]/.test(this.password)) {
+      this.colors = ["yellow", "yellow", "grey"];
+    } else {
+      this.colors = ["green", "grey", "grey"];
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ItemComponent } from './components/item/item.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
